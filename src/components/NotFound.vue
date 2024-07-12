a9b2efe493e3760f225d19c3b348a4976576bfba
KslBPK <3225853821@qq.com>
2024-07-12 16:30:11 +0800

    添加Login页面 和 404页面，配置路由

---
+++ src/components/NotFound.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="NotFound">
+      <h1>{{ msg }}</h1>
+      <h1>{{ warning }}</h1>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'NotFound',
+    data () {
+      return {
+        msg: '404',
+        warning: '您所访问的页面不存在，请你检查一下页面的路径是否正确！！'
+      }
+    }
+  }
+  </script>
+  
+  
+  <style scoped>
+  .NotFound h1{
+    color: red;
+  }
+  </style>
+  
--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,38 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from '@/components/Home'
+// import Home from '@/components/Home'
+// import Login from '@/components/Login'
+import NotFound from '@/components/NotFound'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
+      // 首页使用懒加载
+      // 1.使用import方法（常用）
       path: '/',
-      name: 'HelloWorld',
-      component: Home
-    }
+      name: 'Home',
+      // component: Home
+      component: () => import('@/components/Home')
+      // 2.使用require方法
+      // path: '/',
+      // name: 'Home',
+      // components: require('@/components/Home')
+    },
+    {
+      // 登录页面使用懒加载
+      // 3.使用异步方法（常用）
+      path: '/login',
+      name: 'Login',
+      // component: Login
+      component: resolve => require((['@/components/Login']),resolve)
+    },
+    {
+      path: '*',
+      name: 'NotFound',
+      component: NotFound
+    },
   ],
   mode: 'history'
 })
