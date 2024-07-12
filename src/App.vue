147e9f5526db5ab5c005ff65af0dca27cb79cb55
KslBPK <3225853821@qq.com>
2024-07-12 15:53:52 +0800

    更新

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
     <router-view/>
   </div>
 </template>
@@ -12,6 +11,8 @@ export default {
 </script>
 
 <style>
+/* 引入CSS-reset.css重置样式 */
+@import url('./assets/css/reset.css');
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
