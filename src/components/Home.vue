27eca1357442aabb2d6a591934b19de61adf6831
KslBPK <3225853821@qq.com>
2024-07-13 17:54:31 +0800

    添加菜单组件，添加学生类相关列表

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -4,7 +4,10 @@
     <el-container class="content">
       <div>菜单</div>
       <el-main>
-        <div>主要内容区</div>
+        <div>
+          <!-- 路由出口（没有这个东西看不到界面） -->
+          <!-- <router-view></router-view> -->
+        </div>
         <Footer></Footer>
       </el-main>
     </el-container>
@@ -14,6 +17,7 @@
 <script>
 import Header from './common/Header'
 import Footer from './common/Footer'
+import router from '../router';
 export default {
   components:{
       Header : Header,
---
+++ src/components/student/WorkList.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="worklist">
+      作业列表
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'WorkList',
+    data () {
+      return {
+      }
+    }
+  }
+  </script>
+  
+  <!-- Add "scoped" attribute to limit CSS to this component only -->
+  <style scoped>
+  </style>
+  
